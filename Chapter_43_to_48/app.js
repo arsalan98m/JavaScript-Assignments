8a338d44a18fb407439ce1c738e83a789cf964ae
arsalan manzoor <arsalan1998m@gmail.com>
2020-07-19 12:44:33 +0500

    Updated assignments chapter 43 to 48

--- Chapter_43_to_48/app.js
+++ Chapter_43_to_48/app.js
@@ -6,12 +6,30 @@ function purchase() {
 
 // Task # 3
 
-function deleteData(id) {
-    var element = document.getElementById(id);
-    element.innerHTML = "<td></td>";
+var table = document.getElementById("table");
+var names = ["Jhone", "Doe", "Mark", "James", "abid", "bilal", "tahir", "imran", "usman", "ahmed"];
+var classes = [10, 9, 10, 8, 7, 10, 4, 5, 6, 8];
+
+function showData() {
+    table.innerHTML = `<tr><td>Index</td><td>Names</td><td>Classes</td</tr>`
+    for (var i = 0; i < names.length; i++) {
+        table.innerHTML += `<tr id="${i}"><td id=index >${i}</td><td>${names[i]}</td><td>${classes[i]}</td><td><button onclick="deleteRecord(${i})">Delete</button></td></tr>`;
+    }
 }
 
 
+
+function deleteRecord(e) {
+    names.splice(e, 1);
+    classes.splice(e, 1);
+
+    table.innerHTML += "";
+    showData();
+
+
+
+}
+showData();
 // Task # 5
 
 counter = 0;
