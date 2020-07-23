b5c44f413c95c3e3c429d0c29fc948908eefa461
arsalan manzoor <arsalan1998m@gmail.com>
2020-07-23 22:21:09 +0500

    Updated assignment chapter 52 to 57

--- Chapter_52_to_57/Task # 1 (Modal)/app.js
+++ Chapter_52_to_57/Task # 1 (Modal)/app.js
@@ -1,7 +1,7 @@
 // Task # 1 (Modal)
 
 var images = ['images/others/1.jpg', 'images/others/10.jpg', 'images/others/3.jpg', 'images/others/5.jpg'];
-
+var captions = ['Salar de Uyuni, Bolivia', 'Cinque Terre, Rio Maggiore, Italy', 'Blue Lagoon, Iceland', 'Maldives'];
 function displayImages() {
     for (var i = 0; i < images.length; i++) {
         document.body.innerHTML += `<img src='${images[i]}' width="300px" id="${i}" data-toggle="modal" data-target="#exampleModal" onclick="displayModal(this)" >`
@@ -14,7 +14,8 @@ displayImages();
 function displayModal(e) {
 
     var setImg = document.getElementById("setImage");
-    setImg.innerHTML = `<img src="${e.src}" width="100%" >`
+    setImg.innerHTML = `<figure><img src="${e.src}" width="100%" ><figcaption>${captions[e.id]}</figcaption></figure>`;
+
 
 }
 
